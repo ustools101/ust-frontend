c04af6f0c43beebb5643d32e13a1ed822cf893d5
Godwin Idemudia <rustoshidev@gmail.com>
2025-12-08 20:14:03 +0100

    new

--- next.config.js
+++ next.config.js
@@ -7,8 +7,9 @@ const withPWA = require('next-pwa')({
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
- /* config options here */
- images: {
+  /* config options here */
+  turbopack: {},
+  images: {
   remotePatterns: [
     {
       // Allow any hostname for HTTPS images.
