4817f8fb748b5e25128f29fde7204cd6333ce95f
Godwyn <rustoshidev@gmail.com>
2026-04-18 16:38:44 +0100

    new

--- src/components/TelegramSupport.tsx
+++ src/components/TelegramSupport.tsx
@@ -15,7 +15,7 @@ export default function TelegramSupport() {
       aria-label="Contact Support on Telegram"
     >
       {/* telegram svg icon */}
-      <Image src="/telegram.svg" alt="Telegram" width={24} height={24} />
+      <Image src="/telegram.png" alt="Telegram" width={24} height={24} />
     </Link>
   );
 }
